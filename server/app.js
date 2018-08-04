a2534ed6ac81331800ccdafa79695fc88691e96f
mariogmazza <mariomazza01@gmail.com>
2018-08-04 19:04:34 -0400

    working mongodb on mlab also added mutations

--- server/app.js
+++ server/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
+mongoose.connect('mongodb://pepe:test123@ds113122.mlab.com:13122/graphql-test');
+mongoose.connection.once('open',()=>{
+    console.log('connected to database');
+})
+
+
+
 app.use('/graphql', graphqlHTTP({
-    schema:schema
+    schema:schema,
+    graphiql:true
 }))
 
 app.listen(4000,()=>{
     console.log('Now Listening on port: 4000')
 });
 
+    
